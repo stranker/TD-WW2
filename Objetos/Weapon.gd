19cf1652912e7532eac9db221d4d8cbb46e9406d
Unknown <dani_mrx93@hotmail.com>
2018-02-10 22:50:09 -0300

    Add control

--- Objetos/Weapon.gd
+++ Objetos/Weapon.gd
@@ -2,7 +2,6 @@ extends Node
 
 var clip
 var magazine
-var reloading_time
 var shooting_time
 
 func _ready():
@@ -14,9 +13,6 @@ func set_clip(value):
 func set_magazine(value):
 	magazine = value
 
-func set_reloading(value):
-	reloading_time = value
-
 func set_shooting(value):
 	shooting_time = value
 
@@ -26,15 +22,9 @@ func get_clip():
 func get_magazine():
 	return magazine
 
-func get_reloading():
-	return reloading_time
-
 func get_shooting():
 	return shooting_time
 
 
 func shoot():
-	pass
-
-func reload():
 	pass
